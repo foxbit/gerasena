8e6f92ba4da074557df0f34a3e439f128d66343e
foxbit <angelorosa@gmail.com>
2022-03-21 23:56:10 -0300

    Range operacional

--- js/gerasena.js
+++ js/gerasena.js
@@ -1,52 +1,66 @@
-let divSenaResultArea = document.getElementById("senaResultArea")
+let divSenaResultArea = document.getElementById("senaResultArea");
 
 
-let button = document.querySelector("button");
-        button.onclick = sorteiaNumeros;
 
-        function sorteia() {
+function sorteia() {
+   return Math.floor(Math.random() * 60) + 1;
+}
 
-            return Math.floor(Math.random() * 60) + 1;
 
-        }
 
-        function sorteiaNumeros() {
+function sorteiaNumeros() {
+    
+    let qtdJogos = document.getElementById("myRange").value;
+    let megaSena = [];
+    let numero = 1;
+    let quantidadeMaximaDeNumeros = 6;
 
-            let megaSena = [];
-            let numero = 1;
-            let quantidadeMaximaDeNumeros = 6;
+    while(numero <= quantidadeMaximaDeNumeros) {
 
-            while(numero <= quantidadeMaximaDeNumeros) {
+        let numerosSorteados = sorteia();
+        let achou = false;
 
-                let numerosSorteados = sorteia();
-                let achou = false;
+            for(posicao = 0; posicao < megaSena.length; posicao++) {
 
-                    for(posicao = 0; posicao < megaSena.length; posicao++) {
+                if(megaSena[posicao] == numerosSorteados) {
 
-                        if(megaSena[posicao] == numerosSorteados) {
-
-                            achou = true;
-                            break;
-                        }
-                    }
-
-                    if(achou == false) {
-
-                        megaSena.push(numerosSorteados);
-                        numero++;
-                    }
-
+                    achou = true;
+                    break;
+                }
             }
 
-            megaSena.sort(function(a, b){return a - b});
+            if(achou == false) {
 
-            for (i = 0; i < quantidadeMaximaDeNumeros; i++) {
-                $('<div class="senaResultItem" />').text(megaSena[i]).appendTo(divSenaResultArea);
-              }
-                     
-            
-            divSenaResultArea.style.display = "block";
-            divSenaResultArea.style.display = "flex";
-            
+                megaSena.push(numerosSorteados);
+                numero++;
+            }
+
         }
-        
+
+    megaSena.sort(function(a, b){return a - b});
+
+    let z = 0;
+
+    while (z < qtdJogos) {
+        // Executa 5 vezes, com os valores de passos de 0 a 4.
+        for (i = 0; i < quantidadeMaximaDeNumeros; i++) {
+          $('<div class="senaResultItem" id="numero"/>').text(megaSena[i]).appendTo(divSenaResultArea);
+                 
+          }
+          z++;
+      } 
+                
+    
+    divSenaResultArea.style.display = "block";
+    divSenaResultArea.style.display = "flex";
+    
+}
+
+
+
+/*
+function limpaResultados() {
+    let numeroDiv = document.getElementById("numero");
+    numeroDiv.remove();
+}
+*/
---
+++ js/test.js
@@ -0,0 +1,15 @@
+
+
+function qualvalor() {
+
+    let range = document.getElementById("myRange").value;
+    
+    window.alert(range);
+}
+
+/*
+function myFunction() {
+    let x = document.getElementById("myRange").value;
+    document.getElementById("demo").innerHTML = x;
+  }
+  
